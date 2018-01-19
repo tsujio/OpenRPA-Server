8a5efe6d0fb4405263bbfc12f9c6233ff40f73b7
tsujio <ntsujio@gmail.com>
2018-01-20 00:57:29 +0900

    Use vue-mdc-adapter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import './theme.scss'
 import Vue from 'vue'
+import VueMDCAdapter from 'vue-mdc-adapter'
 import App from './App.vue'
+
+Vue.use(VueMDCAdapter)
 
 new Vue({
   el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'app.js'
   },
   module: {
     rules: [
@@ -16,11 +16,51 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.join(__dirname, 'node_modules')]
+            }
+          }
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader?indentedSyntax',
+            options: {
+              includePaths: [path.join(__dirname, 'node_modules')]
+            }
+          }
+        ],
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
           }
           // other vue-loader options go here
         }
